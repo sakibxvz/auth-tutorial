56127760df4603d91c87ab9d5dc30a375dc518fd
sakibxvz <sakibxvz@gmail.com>
2024-07-27 00:56:47 +0600

    back button css minor edit

--- components/auth/back-button.tsx
+++ components/auth/back-button.tsx
@@ -10,7 +10,7 @@ interface BackButtonProps {
 
 export const BackButton = ({ href, label }: BackButtonProps) => {
 	return (
-		<Button variant='link' className='font-normal w-full' size='sm' asChild>
+		<Button variant='link' className='m-auto font-semibold' size='sm' asChild>
 			<Link href={href}>{label}</Link>
 		</Button>
 	);
