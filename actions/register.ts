109914dc4c298ab43c724a505b89ecaea3133a44
sakibxvz <sakibxvz@gmail.com>
2024-07-27 20:58:20 +0600

    Prisma Connection Done

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 'use server';
 import * as z from 'zod';
 import { RegisterSchema } from '@/schemas';
+import bcrypt from 'bcrypt';
+import { db } from '../lib/db';
+import { getUserByEmail } from '../data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	const validatedFields = RegisterSchema.safeParse(values);
@@ -8,7 +11,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	if (!validatedFields.success) {
 		return { error: 'Invalid Fileds' };
 	}
-	console.log(values);
+	const { email, password, name } = validatedFields.data;
+	const hashedPassword = await bcrypt.hash(password, 10);
 
-	return { success: 'Email Sent !' };
+	const existingUser = await getUserByEmail(email)
+
+	if (existingUser) {
+		return { error: 'Email already in use' };
+	}
+
+	await db.user.create({
+		data: {
+			name,
+			email,
+			password: hashedPassword,
+		},
+	});
+
+	//TODO: Send verification email token
+
+	return { success: 'User Created!' };
 };
